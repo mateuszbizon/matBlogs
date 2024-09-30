a654daa219ab50a9c20f7bd5bce50662351be7f3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-09-30 21:00:30 +0200

    creating reply to comment

--- server/src/types/responses/comment.response.ts
+++ server/src/types/responses/comment.response.ts
@@ -1,5 +1,9 @@
-import { TComment } from "../models"
+import { TComment, TCommentReply } from "../models"
 
 export type TCommentReponse = {
     comment: TComment
+}
+
+export type TCommentReplyResponse = {
+    commentReply: TCommentReply
 }
