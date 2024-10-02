870ebf7a42b7412ce1b2df2e02bfa3b6a8a310ec
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-10-02 15:23:55 +0200

    get post comments route

--- server/src/routes/comment.route.ts
+++ server/src/routes/comment.route.ts
@@ -4,6 +4,7 @@ import { createCommentController } from "../controllers/comments/createComment.c
 import { deleteCommentController } from "../controllers/comments/deleteComment.controller"
 import { createCommentReplyController } from "../controllers/comments/createCommentReply.controller"
 import { deleteCommentReplyController } from "../controllers/comments/deleteCommentReply.controller"
+import { getPostCommentsController } from "../controllers/comments/getPostComments.controller"
 
 const router = express.Router()
 
@@ -11,5 +12,6 @@ router.post("/create-comment/:postId", authenticationMiddleware, createCommentCo
 router.delete("/delete-comment/:commentId", authenticationMiddleware, deleteCommentController)
 router.post("/create-comment-reply/:commentId/:username", authenticationMiddleware, createCommentReplyController)
 router.delete("/delete-comment-reply/:commentReplyId", authenticationMiddleware, deleteCommentReplyController)
+router.get("/get-post-comments/:postId", getPostCommentsController)
 
 export default router
