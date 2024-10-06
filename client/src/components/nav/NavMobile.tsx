f284134d40f6cef812506184c76df8175f61d38d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-10-06 20:51:23 +0200

    added container to nav mobile

--- client/src/components/nav/NavMobile.tsx
+++ client/src/components/nav/NavMobile.tsx
@@ -10,13 +10,15 @@ type NavMobileProps = {
 
 function NavMobile({ navMobileOpen, setNavMobileOpen }: NavMobileProps) {
   return (
-    <div className={`fixed top-0 right-0 ${navMobileOpen ? "translate-x-0" : "translate-x-full"} w-[250px] h-screen bg-white py-2 px-5 z-30 transition-all duration-300 lg:hidden`}>
-        <div className='flex flex-row-reverse'>
-            <button className='p-2 text-dark' onClick={() => setNavMobileOpen(false)}>
-                <div className='size-5'>
-                    <MenuBarsIcon />
-                </div>
-            </button>
+    <div className={`fixed top-0 right-0 ${navMobileOpen ? "translate-x-0" : "translate-x-full"} w-[250px] h-screen bg-white py-2 z-30 transition-all duration-300 lg:hidden`}>
+        <div className='main-container'>
+            <div className='flex flex-row-reverse'>
+                <button className='p-2 text-dark' onClick={() => setNavMobileOpen(false)}>
+                    <div className='size-5'>
+                        <MenuBarsIcon />
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
   )
