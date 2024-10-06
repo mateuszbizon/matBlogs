31014bb3f87cf4309ad8c946c30234909b4a7942
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-10-06 20:53:04 +0200

    body background color

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.className}`}
+        className={`${montserrat.className} bg-light`}
       >
         <Nav />
         {children}
