0d7bf77b804280962b41cb2ae90b9d472b224e08
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-10-07 13:41:35 +0200

    dropdown for serch in nav in mobile

--- client/src/components/nav/NavSearchItem.tsx
+++ client/src/components/nav/NavSearchItem.tsx
@@ -7,15 +7,17 @@ function NavSearchItem() {
     const [searchOpen, setSearchOpen] = useState(false)
 
   return (
-    <>
-        <div className='relative hidden lg:block'>
-            <button className='nav-link' onClick={() => setSearchOpen(prev => !prev)}>Search</button>
+    <div className='relative'>
+        <button className='nav-link' onClick={() => setSearchOpen(prev => !prev)}>Search</button>
 
-            <div className={`absolute -left-1/2 top-[120%] bg-white p-2 rounded-lg z-30 whitespace-nowrap ${searchOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
-                <NavSearchItemsList />
-            </div>
+        <div className={`absolute -left-1/2 top-[120%] bg-white p-3 rounded-lg z-30 whitespace-nowrap hidden lg:block ${searchOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
+            <NavSearchItemsList />
         </div>
-    </>
+
+        <div className={`pl-2 mt-2 lg:hidden overflow-hidden ${searchOpen ? "max-h-[500px]" : "max-h-0"} transition-all duration-500`}>
+            <NavSearchItemsList />
+        </div>
+    </div>
   )
 }
 
