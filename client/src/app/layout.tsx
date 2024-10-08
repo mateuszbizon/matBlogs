46dc6a9c75ba8f5e272101049034ded6e23faa6d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-10-08 12:43:38 +0200

    tanstack query setup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google"
 import "./globals.css";
 import Nav from "@/components/nav/Nav";
+import Providers from "@/components/Providers";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -19,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${montserrat.className} bg-light`}
-      >
-        <Nav />
-        {children}
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body
+          className={`${montserrat.className} bg-light`}
+        >
+          <Nav />
+          {children}
+        </body>
+      </html>
+    </Providers>
   );
 }
