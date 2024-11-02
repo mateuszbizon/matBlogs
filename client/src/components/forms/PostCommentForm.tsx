4640c8b45bb2b37856a5b96bdaa1d9f9c5df566d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-02 18:58:14 +0100

    post comment form only for signed in user

--- client/src/components/forms/PostCommentForm.tsx
+++ client/src/components/forms/PostCommentForm.tsx
@@ -10,12 +10,15 @@ import { commentSchema, TCommentSchema } from '@/validations/commentSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import InputErrorMessage from './InputErrorMessage'
 import useCreatePostComment from '@/hooks/useCreatePostComment'
+import Link from 'next/link'
+import { useUserAuth } from '@/context/UserAuthContext'
 
 type PostCommentFormProps = {
     postId: string;
 }
 
 function PostCommentForm({ postId }: PostCommentFormProps) {
+    const { isSignedIn } = useUserAuth()
     const { handleCreatePostComment, isPendingCreatePostComment } = useCreatePostComment()
     const { register, handleSubmit, formState: { errors }, reset } = useForm<TCommentSchema>({
         resolver: zodResolver(commentSchema),
@@ -33,7 +36,9 @@ function PostCommentForm({ postId }: PostCommentFormProps) {
     }
 
   return (
-    <form className='w-full max-w-[700px]' onSubmit={handleSubmit(onSubmit)}>
+    <form className='relative w-full max-w-[700px]' onSubmit={handleSubmit(onSubmit)}>
+        <Link href={"/sign-in"} className={`absolute inset-0 z-[5] ${isSignedIn && "pointer-events-none"}`}></Link>
+
         <FormBox>
             <Label htmlFor='content'>Add comment</Label>
             <Textarea id="content" variant={errors.content && "primary-error"} {...register("content")} placeholder='Comment content'></Textarea>
