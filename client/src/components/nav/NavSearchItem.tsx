521c0040f6b13ea7726a4781ac6ed8cca120ce83
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-05 15:54:53 +0100

    nav search popover

--- client/src/components/nav/NavSearchItem.tsx
+++ client/src/components/nav/NavSearchItem.tsx
@@ -2,20 +2,34 @@
 
 import React, { useState } from 'react'
 import NavSearchItemsList from './NavSearchItemsList'
+import { Popover } from 'react-tiny-popover'
 
 function NavSearchItem() {
     const [searchOpen, setSearchOpen] = useState(false)
+    const [searchOpenMobile, setSearchOpenMobile] = useState(false)
 
   return (
-    <div className='relative'>
-        <button className='nav-link' onClick={() => setSearchOpen(prev => !prev)}>Search</button>
-
-        <div className={`absolute -left-1/2 top-[120%] bg-white p-3 rounded-lg z-30 whitespace-nowrap hidden lg:block ${searchOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} transition-all duration-300`}>
-            <NavSearchItemsList />
-        </div>
-
-        <div className={`pl-2 mt-2 lg:hidden overflow-hidden ${searchOpen ? "max-h-[500px]" : "max-h-0"} transition-all duration-500`}>
-            <NavSearchItemsList />
+    <div>
+        <Popover
+          isOpen={searchOpen}
+          positions={['bottom', 'left']}
+          onClickOutside={() => setSearchOpen(false)}
+          padding={10}
+          containerClassName='hidden lg:block z-30'
+          content={() => (
+            <div className={`bg-white p-3 rounded-lg`}>
+                <NavSearchItemsList />
+            </div>
+          )}
+        >
+          <button className='nav-link hidden lg:block' onClick={() => setSearchOpen(prev => !prev)}>Search</button>
+        </Popover>
+        
+        <div className='lg:hidden'>
+          <button className='nav-link' onClick={() => setSearchOpenMobile(prev => !prev)}>Search</button>
+          <div className={`pl-2 mt-2 overflow-hidden ${searchOpenMobile ? "max-h-[500px]" : "max-h-0"} transition-all duration-500`}>
+              <NavSearchItemsList />
+          </div>
         </div>
     </div>
   )
