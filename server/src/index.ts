0382fb65ae8e102fab9682c71f992145120cdad0
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-23 18:16:40 +0100

    more cors options

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,16 @@ app.use(cookieParser())
 app.use(cors({
     origin: ["http://localhost:3000", "https://mat-blogs.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 }))
+
+app.options("*", cors({
+    origin: ["http://localhost:3000", "https://mat-blogs.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+}));
 
 app.use("/users", userRoutes)
 app.use("/posts", postRoutes)
